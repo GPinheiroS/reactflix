8a10bd7c5f2fb7b0d276dd9cc36589040599d66a
GPinheiro <gabriel-p-santos@hotmaol.com>
2022-03-30 17:55:31 -0300

    Função de adicionar canais e videos

--- react-flix/src/components/AddCanal/AddModal.tsx
+++ react-flix/src/components/AddCanal/AddModal.tsx
@@ -1,26 +1,96 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './AddModal.css'
+import { connect , RootStateOrAny, useDispatch } from 'react-redux';
+import * as videoData from '../../store/videoData'
 
-const AddModal = () => {
+
+const AddModal = ({select}:any) => {
+    const dispatch = useDispatch()
+    const [ListaVideo, SetListaVideo]:any = useState([])
+
     const CloseModal = () =>{
         const modal:any = document.querySelector('.addModal')
         modal.classList.remove('md-show')
-     }
+    } 
+
+    const AddVideo = () =>{
+        const inputTituloVideo:any = document.querySelector('#TituloVideo')
+        const inputUrlVideo:any = document.querySelector('#LinkVideo')
+        console.log('Lista de Videos:', ListaVideo)
+        if(inputTituloVideo.value != '' && inputUrlVideo.value != ''){
+        console.log('passou')
+        let NovoVideo ={
+        id: (ListaVideo.length + 1),
+        title: inputTituloVideo.value,
+        local: inputUrlVideo.value}
+        SetListaVideo([...ListaVideo, NovoVideo])
+        inputTituloVideo.value = ''
+        inputUrlVideo.value=''
+        }
+        
+            
+    } 
+
+    const SalvarCanal = () =>{
+    const inputNomeCanal:any = document.querySelector('#NomeCanal')
+    const inputTituloVideo:any = document.querySelector('#TituloVideo')
+    const inputUrlVideo:any = document.querySelector('#LinkVideo')
+    
+    let NomeCanal = inputNomeCanal.value
+    let TituloVideo = inputTituloVideo.value
+    let UrlVideo = inputUrlVideo.value
+    const novoCanal = {
+        id: (select.length + 1),
+            nome: NomeCanal,
+                videos:[...ListaVideo]
+    }
+    const ListaNova = [...select, novoCanal]
+    SetListaVideo([])
+    inputNomeCanal.value = ''
+    inputTituloVideo.value = ''
+    inputUrlVideo.value=''
+    CloseModal()
+    return{
+        type: 'SALVAR_CANAL',
+        ListaNova
+    }      
+}
     return (
         <div className='addModal'>
-            <input type="Canal" placeholder='Nome do Canal' />
-            <input type="NomeVideo" placeholder='Título do Video' />
-            <input type="LinkVideo" placeholder='Link do Video' />
+            <div className='row'>
+            <label htmlFor="NomeCanal">Nome do Canal</label>
+            <input type="text" id='NomeCanal' placeholder='Nome do Canal' />
+            </div>
+            <div className='adicionarVideo'>
+                <div className='row'>
+                    <label htmlFor="TituloVideo">Título do Video</label>
+                    <input type="text" id='TituloVideo' placeholder='Título do Video' />
+                </div>
+                <div className='row'>
+                    <label htmlFor="LinkVideo">Link do Video</label>
+                    <input type="url" id='LinkVideo' placeholder='Link do Video' />
+                </div>
+                <div className='btn addVideo' onClick={AddVideo}>+</div>
+            </div>
+            <div className='row'>
+                <ul>
+                {ListaVideo.map((video:any) =>(
+                    <li key={video.id}>{video.title}</li>
+                ))}
+                </ul>
+            </div>
+            <div className='row'>
             <div className='btnBox'>
-                <div className='btn'>
+                <div className='btn' onClick={() => dispatch(SalvarCanal())}>
                 Salvar
                 </div>
                 <div className='btn Cancelar' onClick={CloseModal}>
                 Cancelar
                 </div>
             </div>
+            </div>
         </div>
     );
 };
 
-export default AddModal;
+export default connect((state:RootStateOrAny) => ({select:state.select}))(AddModal);
--- react-flix/src/store/index.tsx
+++ react-flix/src/store/index.tsx
@@ -1,14 +1,27 @@
+import { act } from "@testing-library/react";
 import { Reducer, useReducer } from "react";
 import { AnyAction, createStore } from "redux";
 import * as videoData from './videoData'
 
 const reducer = (state:videoData.state = videoData.initialState , action:AnyAction) => {
-    if(action.type === "TROCAR_VIDEO"){
-        console.log(action)
-        console.log(state)
-        return { ...state, atual: action.video }
-        } 
-    return state ;
+    console.log('Action=',action)
+    console.log('State=',state)
+    switch(action.type){
+        case "TROCAR_VIDEO":
+        return  { ...state, atual: action.video }
+        break;
+        case "SALVAR_CANAL":
+        return {...state, select: action.ListaNova}
+        break;
+        default:
+        return state
+    }
+    // if(action.type === "TROCAR_VIDEO"){
+    //     console.log(action)
+    //     console.log(state)
+    //     return { ...state, atual: action.video }
+    //     } 
+    // return state ;
 }
 
 const store = createStore(reducer);
