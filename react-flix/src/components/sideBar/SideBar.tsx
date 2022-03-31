2427a99bc273dd524354615641df2713378daf7b
GPinheiro <gabriel-p-santos@hotmaol.com>
2022-03-31 10:37:17 -0300

    Side Bar Accordion update

--- react-flix/src/components/sideBar/SideBar.tsx
+++ react-flix/src/components/sideBar/SideBar.tsx
@@ -1,21 +1,29 @@
-import React, {FunctionComponent} from 'react';
+import React, {FunctionComponent, useState} from 'react';
 import './SideBar.css'
 import { connect , RootStateOrAny } from 'react-redux';
 import { useDispatch } from 'react-redux'
 
 
 let aberto:boolean = false
-const ToggleVideoMenu = (e:any) =>{ 
-    const menu = e.target.nextSibling.firstChild
-    if(e){
-        aberto= !aberto
-    }
-    if(aberto === true){
-        menu.style.display = "block"
-    } else { menu.style.display = "none"}
-}
+// const ToggleVideoMenu = (e:any) =>{ 
+//     const menu = e.target.nextSibling.firstChild
+//     if(e){
+//         aberto= !aberto
+//     }
+//     if(aberto === true){
+//         menu.style.display = "block"
+//     } else { menu.style.display = "none"}
+// }
 
 const SideBar:FunctionComponent = ({select}:any) => {
+    const [accordionArray, setAccordion] = useState(Array(select.length).fill(false))
+
+    const ToggleVideoMenu = (index:number) =>{
+        const newArray = [...accordionArray]
+        newArray[index] = !newArray[index] 
+        setAccordion(newArray); 
+    }
+
     const dispatch = useDispatch()
     function trocarVideo(video:any){
     return{
@@ -26,10 +34,10 @@ const SideBar:FunctionComponent = ({select}:any) => {
     return (
         <div className='sideBar'>
         <ul className='barraSelecao'>
-            {select.map( (youtuber:any) => (
+            {select.map( (youtuber:any, index:number) => (
                 <li key={youtuber.id} className='selecao'>
-                    <button className='selectButton' onClick={(e) => ToggleVideoMenu(e)}>{youtuber.nome}</button>
-                    <div className='menuVideo'>
+                    <button className='selectButton' onClick={()=>ToggleVideoMenu(index)}>{youtuber.nome}</button>
+                    <div className={accordionArray[index]? 'menuVideo open':  'menuVideo'}>
                     <ul>
                     {youtuber.videos.map( (videos:any)  => (
                         <li key={videos.id} className='selectButton' onClick={()=> dispatch(trocarVideo(videos))}>{videos.title}
